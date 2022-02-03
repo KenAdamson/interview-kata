44901d8b7fa7bbe1b92bb0d7b8cfd1731dc10ca4
Sean Boehnke <boehnke3131@gmail.com>
2022-02-03 16:15:51 -0600

    First code test.

--- c#/wordle-kata/wordle-word/Program.cs
+++ c#/wordle-kata/wordle-word/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace wordle_word
 {
@@ -6,7 +8,62 @@ namespace wordle_word
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var wordDict = new Dictionary<char, float>();
+            var words = System.IO.File.ReadAllLines("./sgb-words.txt");
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    if (!wordDict.ContainsKey(c))
+                    {
+                        wordDict[c] = 0;
+                    }
+
+                    wordDict[c] += 1;
+                }
+            }
+
+            var scale = 0.5f;
+            var ordered = wordDict.OrderByDescending(w => w.Value);
+            var vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+            foreach (var item in ordered)
+            {
+                if (vowels.Contains(item.Key))
+                {
+                    var val = item.Value + item.Value * scale;
+                    wordDict[item.Key] = val;
+                }
+                // var line = $"{item.Key}: {item.Value}";
+                // Console.WriteLine(line);
+            }
+
+            var wordScores = new Dictionary<string, float>();
+            foreach (var word in words)
+            {
+                if (!wordScores.ContainsKey(word))
+                {
+                    wordScores[word] = 0;
+                }
+
+                wordScores[word] = GetScoreForWord(word, wordDict);
+            }
+
+            var orderedByScores = wordScores.OrderByDescending(ws => ws.Value);
+            foreach (var item in orderedByScores.Take(10))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+
+        static float GetScoreForWord(string word, Dictionary<char, float> wordDict)
+        {
+            var score = 0f;
+            var currentWord = word.Distinct();
+            foreach (var c in currentWord)
+            {
+                score += wordDict[c];
+            }
+            return score;
         }
     }
 }
